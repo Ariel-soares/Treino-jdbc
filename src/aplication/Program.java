6f5c6f354832882170bc8abb55557c42b8e0a8c7
Ariel Soares <ariel.sfranco@gmail.com>
2024-02-22 20:36:20 -0300

    Update

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,38 @@
 package aplication;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement(0, 0);
+			
+			rs = st.executeQuery("Select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		} 
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+		}
 	}
 
 }
